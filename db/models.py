f65dcdbf2a897ed71399e755196157cf6389a3f0
Jakub <jakubmichalski89@fgmail.com>
2025-08-22 13:02:44 +0200

    'Solution'

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
-        return self.name
+        return str(self.name)
 
 
 class Guild(models.Model):
@@ -13,7 +13,7 @@ class Guild(models.Model):
     description = models.TextField(blank=True, null=True)
 
     def __str__(self) -> str:
-        return self.name
+        return str(self.name)
 
 
 class Skill(models.Model):
@@ -25,17 +25,19 @@ class Skill(models.Model):
         unique_together = ("name", "race")
 
     def __str__(self) -> str:
-        return self.name
+        return str(self.name)
 
 
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.CharField(max_length=255, unique=True)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
+    guild = models.ForeignKey(
+        Guild, on_delete=models.SET_NULL, null=True, blank=True
+    )
     bio = models.CharField(max_length=255, blank=True, default="")
     skills = models.ManyToManyField(Skill, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return self.nickname
+        return str(self.nickname)
